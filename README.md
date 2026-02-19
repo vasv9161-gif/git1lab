6cf6e4cc450712bfc89ce0c096c7ce9d145b98d2
Andrey <flaminghxtcheetos@gmail.com>
2026-02-20 00:15:49 +0300

    "Начальный коммит: добавлены Program.cs и README"

---
+++ README.md
@@ -0,0 +1,6 @@
+git 1 lab
+
+Vasilenko Andrey
+
+2411-0101.1
+
---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace lab1git
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
